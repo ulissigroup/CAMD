aecc3698143ded37b4e96d80d3cd2e4e54adaeaa
Kirby Broderick <kirbybroderick@gmail.com>
2022-12-15 16:58:20 +0000

    bugfix

--- camd/campaigns/base.py
+++ camd/campaigns/base.py
@@ -199,7 +199,7 @@ class Campaign(MSONable):
         )
 
         # Campaign stopper if agent doesn't have anything to suggest.
-        if (len(suggested_experiments) == 0) or (suggested_experiments is None):
+        if (suggested_experiments is None) or (len(suggested_experiments) == 0):
             self.finalize()
             self.logger.info("No agent suggestions. Stopping the loop.")
             return False
